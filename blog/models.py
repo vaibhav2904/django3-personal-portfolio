7e513d01e55d7589f085c666349cc723cfe5ce93
vaibhav <vaibhav.tanw@gmail.com>
2021-03-13 18:04:16 +0530

    First commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=100)
+
+    description = models.TextField(max_length=256)
+
+    date =  models.DateField()
+
+    def __str__(self):
+        return self.title
