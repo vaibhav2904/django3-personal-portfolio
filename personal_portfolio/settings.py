59f7158181e78d8f06839119e8f07c65366b6823
vaibhav <vaibhav.tanw@gmail.com>
2021-03-13 22:00:41 +0530

    new local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT= BASE_DIR / 'db.static'
 MEDIA_URL='/media/'
 MEDIA_ROOT = BASE_DIR / 'db.media'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('Looks like no local file, you must be on production')
